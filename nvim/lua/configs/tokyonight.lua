cf938aaea8c12a10e3dfc37e288eb7eb791991f5
atmatm9182 <atmatm9182@gmail.com>
2024-07-24 23:19:39 +0200

    [nvim] Change the keywords in `tokyonight` theme to not be italic

--- nvim/lua/configs/tokyonight.lua
+++ nvim/lua/configs/tokyonight.lua
@@ -2,10 +2,16 @@ return {
 	style = "night",
 	transparent = false,
 
+	styles = {
+		comments = { italic = true },
+		keywords = { italic = false },
+	},
+
 	on_highlights = function(highlights, c)
 		highlights.Boolean = { bold = true, fg = c.orange }
 		highlights.Special = { bold = true, fg = c.magenta }
 
+		highlights["@keyword.modifier"] = { fg = c.magenta }
 		highlights["@lsp.type.macro"] = { bold = true, fg = c.purple }
 		highlights["@function.builtin"] = { bold = true, fg = c.purple }
 		highlights["@variable.builtin"] = { italic = true, fg = c.red }
