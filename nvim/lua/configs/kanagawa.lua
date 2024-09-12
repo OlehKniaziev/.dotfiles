85f8d6c1ad420391e3450510d96e25f13b6de78a
atmatm9182 <atmatm9182@gmail.com>
2024-09-12 16:03:01 +0200

    [nvim] customize kanagawa theme highlights

--- nvim/lua/configs/kanagawa.lua
+++ nvim/lua/configs/kanagawa.lua
@@ -1,4 +1,7 @@
 return {
+	compile = false,
+	undercurl = true,
+	theme = "wave",
 	colors = {
 		theme = {
 			all = {
@@ -8,5 +11,31 @@ return {
 			},
 		},
 	},
-	-- transparent = true,
+
+	overrides = function(c)
+		local p = c.palette
+
+		return {
+			-- Special = { bold = true, fg = c.magenta },
+
+			["@keyword.modifier"] = { fg = p.peachRed },
+			["@lsp.type.modifier"] = { fg = p.peachRed },
+
+			["@function.builtin"] = { bold = true, fg = p.surimiOrange },
+			["@variable.builtin"] = { italic = true, fg = p.waveRed },
+			["@constant.builtin"] = { bold = true, fg = p.springBlue },
+
+			["@module"] = { italic = true, fg = p.sakuraPink },
+			["@lsp.typemod.namespace"] = { italic = true, fg = p.sakuraPink },
+
+			["@lsp.type.enumMember"] = { bold = true, fg = p.springBlue },
+
+			["@lsp.type.interface"] = { fg = p.lightBlue },
+			["@lsp.mod.trait"] = { fg = p.lightBlue },
+
+			["@lsp.type.macro"] = { bold = true, fg = p.peachRed },
+
+			["@lsp.type.decorator"] = { fg = p.sakuraPink },
+		}
+	end,
 }
