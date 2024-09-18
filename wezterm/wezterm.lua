c1b65b65c09677f34c1c1bc1a57fb6816c310006
atmatm9182 <atmatm9182@gmail.com>
2024-09-18 19:16:16 +0200

    [wezterm] adjust the font size a wee bit more

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -35,7 +35,7 @@ config.font_rules = {
 config.freetype_load_flags = "NO_HINTING"
 config.front_end = "WebGpu"
 
-config.font_size = 22
+config.font_size = 20
 -- config.harfbuzz_features =
 -- 	{ "calt", "liga", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "ss09", "zero" }
 -- config.harfbuzz_features = { "ss01", "zero" }
