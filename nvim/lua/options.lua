4c9cefb02da539df2052fb3c6f6b5f697b7bbe7b
atmatm9182 <atmatm9182@gmail.com>
2024-09-30 15:33:06 +0200

    [nvim] make default search case-insensitive

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,6 +4,9 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.signcolumn = "yes"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 
