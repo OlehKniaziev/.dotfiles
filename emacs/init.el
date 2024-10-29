3611d7be3125bc04563ecf8df462aa357e1a548d
atmatm9182 <atmatm9182@gmail.com>
2024-10-29 18:18:33 +0100

    [emacs] de-bloat the config ☺️

--- emacs/init.el
+++ emacs/init.el
@@ -44,82 +44,7 @@
 (elpaca elpaca-use-package
   (elpaca-use-package-mode))
 
-(set-face-attribute 'default nil :weight 'normal :family "Iosevka Term" :height 190)
-
-(defvar leader-key "SPC")
-
-(defvar key-blacklist `(,leader-key))
-;; (add-to-list 'key-blacklist leader-key)
-
-(defun --evil-normal-bind (keys callback)
-  "Define the evil key binding in the normal mode."
-  (evil-define-key nil evil-normal-state-map (kbd keys) callback)
-  (if (not (boundp 'evil-collection-key-blacklist))
-      (setq evil-collection-key-blacklist nil))
-  (add-to-list 'key-blacklist keys))
-
-(defun --evil-insert-bind (keys callback)
-  "Define the evil key binding in the insert mode."
-  (evil-define-key nil evil-insert-state-map (kbd keys) callback)
-  (if (not (boundp 'evil-collection-key-blacklist))
-      (setq evil-collection-key-blacklist nil))
-  (add-to-list 'key-blacklist keys))
-
-(defun kill-current-buffer ()
-  (interactive)
-  (kill-buffer (buffer-name)))
-
-(use-package evil
-  :ensure t
-  
-  :custom
-  (evil-undo-system 'undo-redo)
-  
-  :init
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-integration t)
-  
-  :config
-  ;; bindings
-  (evil-set-leader 'normal (kbd leader-key))
-
-  (--evil-normal-bind "K" 'eldoc-box-help-at-point)
-  (--evil-normal-bind "<leader>s" 'consult-imenu)
-  (--evil-normal-bind "<leader>S" 'consult-imenu-multi)
-
-  (--evil-normal-bind "<leader>fr" 'recentf-open)
-  (--evil-normal-bind "<leader>ff" 'consult-fd)
-  (--evil-normal-bind "<leader>fg" 'consult-ripgrep)
-  (--evil-normal-bind "<leader>fG" 'consult-git-grep)
-  (--evil-normal-bind "<leader>fb" 'consult-buffer)
-
-  (--evil-normal-bind "<leader>ca" 'eglot-code-actions)
-  (--evil-normal-bind "<leader>cr" 'eglot-rename)
-  (--evil-normal-bind "<leader>cf" 'eglot-format)
-
-  (--evil-normal-bind "<leader>cx" 'flycheck-list-errors)
-
-  (--evil-normal-bind "]d" 'flycheck-next-error)
-  (--evil-normal-bind "[d" 'flycheck-previous-error)
-
-  (--evil-normal-bind "<leader>oc" 'org-capture)
-  (--evil-normal-bind "<leader>oa" 'org-agenda)
-
-  (--evil-insert-bind "C-y" 'corfu-send)
-
-  (--evil-normal-bind "<leader>bd" 'kill-current-buffer)
-
-  (evil-mode 1))
-
-(use-package evil-collection
-  :ensure t
-
-  :init
-  (setq evil-collection-key-blacklist key-blacklist)
-  
-  :config
-  (evil-collection-init))
+(set-face-attribute 'default nil :weight 'normal :family "Iosevka Classy" :height 210)
 
 (use-package org
   :defer t
@@ -138,24 +63,7 @@
 (use-package transient
   :ensure t)
 
-(use-package git-gutter
-  :ensure t
-  :config
-  (setq git-gutter:update-interval 0.2)
-
-  ;; disable the git-gutter mode on windows because the 'diff'
-  ;; command is very slow for whatever reason
-  (if (not (eq system-type 'windows-nt))
-      (global-git-gutter-mode 1)))
-
-(use-package git-gutter-fringe
-  :ensure t
-  :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
-
-(use-package vterm
+(use-package eat
   :ensure t
   :defer t)
 
@@ -174,12 +82,6 @@
 (use-package markdown-mode
   :ensure t)
 
-(use-package eldoc-box
-  :ensure t
-
-  :custom-face
-  (eldoc-box-body ((t (:height 170 :inherit default)))))
-
 (use-package haskell-mode
   :ensure t
   :hook
@@ -187,33 +89,6 @@
 
 (use-package tuareg
   :ensure t)
-
-(use-package eglot
-  :hook
-  ((c-mode . eglot-ensure)
-   (c-ts-mode . eglot-ensure)
-   (c++-mode . eglot-ensure)
-   (js-mode . eglot-ensure)
-   (js-ts-mode . eglot-ensure)
-   (tuareg-mode . eglot-ensure)
-   (haskell-mode . eglot-ensure)
-   (rust-ts-mode . eglot-ensure)
-   (go-ts-mode . eglot-ensure))
-
-  :init
-  (setq eglot-confirm-server-initiated-edits nil)
-  (setq eglot-ignored-server-capabilities
-        '(:inlayHintProvider))
-
-  :config
-  (advice-add 'eglot-completion-at-point :around #'cape-wrap-noninterruptible))
-  ;; (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster))
-
-(use-package flycheck-eglot
-  :ensure t
-
-  :config
-  (global-flycheck-eglot-mode))
 
 (use-package orderless
   :ensure t
@@ -232,30 +107,6 @@
   :init
   (vertico-mode))
 
-(use-package corfu
-  :ensure t
-  
-  :custom
-  (setq corfu-auto-delay 0.3)
-  (corfu-cycle t)
-  (corfu-auto t)
-  (corfu-scroll-margin 3)
-
-  :config
-  (keymap-unset corfu-map "RET")
-  (keymap-unset corfu-map "TAB")
-  
-  :init
-  (global-corfu-mode))
-
-(use-package cape
-  :ensure t
-  
-  :init
-  (add-hook 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-file)
-  (add-hook 'completion-at-point-functions #'cape-elisp-block))
-
 (use-package yasnippet
   :ensure t
 
@@ -270,7 +121,7 @@
 
   :bind
   (:map projectile-mode-map
-	    ("<leader>p" . projectile-command-map))
+	    ("C-c p" . projectile-command-map))
   
   :init
   (projectile-mode 1))
@@ -327,7 +178,6 @@
   :ensure t
 
   :config
-  (load-theme 'ef-summer t)
   (set-face-underline 'ef-themes-underline-warning '(:style line :color "#c0b000"))
   (set-face-underline 'ef-themes-underline-error '(:style line :color "#df2f2f"))
   (set-face-underline 'ef-themes-underline-info '(:style line :color "#22b022")))
@@ -340,8 +190,7 @@
 
 (elpaca-wait)
 
-(set-face-attribute 'eglot-diagnostic-tag-unnecessary-face nil :slant 'italic :inherit 'shadow)
-(set-face-underline 'eglot-highlight-symbol-face t)
+(load-theme 'modus-operandi t)
 
 (set-face-attribute 'mode-line nil
                     :box '(:color "#403f3f" :line-width 2))
