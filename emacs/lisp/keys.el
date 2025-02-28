b3ea358ebede8cb3b3933d73482444a67a50c006
atmatm9182 <atmatm9182@gmail.com>
2025-02-28 19:51:29 +0100

    [emacs] added windmove keybinds

--- emacs/lisp/keys.el
+++ emacs/lisp/keys.el
@@ -4,10 +4,16 @@
 
 (require 'vc-git)
 
-;; Mappings
+;; Misc mappings
 (keymap-global-set "C-c f r" 'recentf-open)
 (keymap-global-set "C-c f p" 'find-file-at-point)
 (keymap-global-set "C-c s g" 'rgrep)
 (keymap-global-set "C-c s G" 'vc-git-grep)
+
+;; Window mappings
+(keymap-global-set "C-c w h" 'windmove-left)
+(keymap-global-set "C-c w j" 'windmove-down)
+(keymap-global-set "C-c w k" 'windmove-up)
+(keymap-global-set "C-c w l" 'windmove-right)
 
 (provide 'keys)
