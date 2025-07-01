371d29021c255516bbd4700cefc9a2480a156587
OlehKniaziev <atmatm9182@gmail.com>
2025-07-01 15:04:45 +0200

    [emacs] prevent custom from ruining my init file

--- emacs/init.el
+++ emacs/init.el
@@ -121,6 +121,9 @@
                        (vc-mode vc-mode)
                        " %m "))))))
 
+  ;; Custom
+  (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
   ;; Dired
   (setq dired-dwim-target t)
   ;; Misc
@@ -305,16 +308,5 @@
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages '((odin-mode :url "https://git.sr.ht/~mgmarlow/odin-mode"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(load custom-file)
