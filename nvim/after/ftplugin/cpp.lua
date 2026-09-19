ca77fd9c5368fb66367c6a8c47dcbb6533913f46
OlehKniaziev <atmatm9182@gmail.com>
2026-09-19 20:14:59 +0200

    [nvim] bump

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,4 +1,4 @@
 vim.opt_local.cinoptions:append("N-s")
 
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
+-- vim.bo.tabstop = 2
+-- vim.bo.shiftwidth = 2
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -19,7 +19,7 @@ vim.opt.list = true
 
 vim.opt.cursorline = true
 
-vim.opt.guicursor = "n-v-i:block-blinkon0,r-o:hor100"
+-- vim.opt.guicursor = "n-v-i:block-blinkon0,r-o:hor100"
 
 vim.opt.guifont = "Codelia Ligatures:h28"
 vim.g.neovide_scale_factor = 0.7
